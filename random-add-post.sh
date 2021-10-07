d5a46e16adf6ff30c849caae5213a110b4a3ec2c
Arthur Ace <arthur4it@gmail.com>
2021-10-07 21:01:35 +0800

    new diary at 20211007-210135

--- random-add-post.sh
+++ random-add-post.sh
@@ -1,10 +1,19 @@
 set -ex
+
 timestamp=$(date +%Y%m%d-%H%M%S)
 diary=diary-${timestamp}.md
 diary_path=content/posts/$diary
 hugo new posts/$diary
+
+# macos sed is not gnu-sed, special treat is needed.
+if [ "$(uname)" == "Darwin" ]; then
+  sed -i '-to-do-remove' '/draft:/s/true/false/' $diary_path
+  rm $diary_path-to-do-remove
+else
+  sed -i '/draft:/s/true/false/' $diary_path
+fi
+
 echo $(fortune) >> $diary_path
 
 git add $diary_path
-git commit -m"new diary at ${timestamp}"
-
+git commit -m "new diary at ${timestamp}"
